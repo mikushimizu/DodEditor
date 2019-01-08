d79c74df7597f4e7e86c4dd3d9f363066c208ac4
mikushimizu <shimizu.miku43239@gmail.com>
2019-01-09 00:27:35 +0900

    IDをPOSTできた

--- pic.php
+++ pic.php
@@ -1,3 +1,21 @@
+<?php
+  session_start();
+
+  if (isset($_POST["bitmap"])) {
+    $bitmap = $_POST["bitmap"];
+	//＊＊＊認証処理＊＊＊
+	/*
+    $pdo = new PDO("sqlite:myblog.sqlite");
+    $st = $pdo->prepare("select * from user where username = ?;");
+    $st->execute(array($username));
+	$user_on_db = $st->fetch();
+	*/
+  }
+  	echo "<pre>";
+	var_dump($bitmap); //var_dump 配列の中身を出力する
+	echo "</pre>";
+?>
+
 <html><head><link rel="stylesheet" id="coToolbarStyle" href="chrome-extension://cjabmdjcfcfdmffimndhafhblfmpjdpe/toolbar/styles/placeholder.css" type="text/css">
 <script type="text/javascript" id="cosymantecbfw_removeToolbar">
 (function () {				var toolbarElement = {},					parent = {},					interval = 0,					retryCount = 0,					isRemoved = false;				if (window.location.protocol === 'file:') {					interval = window.setInterval(function () {						toolbarElement = document.getElementById('coFrameDiv');						if (toolbarElement) {							parent = toolbarElement.parentNode;							if (parent) {								parent.removeChild(toolbarElement);								isRemoved = true;								if (document.body && document.body.style) {									document.body.style.setProperty('margin-top', '0px', 'important');								}							}						}						retryCount += 1;						if (retryCount > 10 || isRemoved) {							window.clearInterval(interval);						}					}, 10);				}			})();
@@ -9,6 +27,8 @@
 <h2>完成！</h2>
 <!--ここでMini画像データを取得し、名前を付けて表示したい-->
 <img src="bilder/0000027877.png">
+<script>console.log("コンソールできたよ！");
+</script>
 
 <br><br>
 <a href="galerie.php">gallery</a>に保存されました. 
