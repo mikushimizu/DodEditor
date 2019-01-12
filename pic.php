c7b6f357a8a059f5556143d5a7d5582d540d4e90
mikushimizu <shimizu.miku43239@gmail.com>
2019-01-13 01:17:16 +0900

    waiwai

--- pic.php
+++ pic.php
@@ -10,7 +10,7 @@
 		$_SESSION["id"]=$id;
 		$_SESSION["info"]=$info;
 		$_SESSION["user"]=$user;
-	} 	      
+	}
 
   if (isset($_POST["bitmap"]) && isset($_POST["info"])) {
 	$bitmap = $_POST["bitmap"];
@@ -26,11 +26,11 @@
 
   //bitmapを配列bitmap_arrayにいれる
 	$bitmap_array = explode(",", $bitmap); 
-	echo (count($bitmap_array)); //配列の数
+	//echo (count($bitmap_array)); //配列の数
 	//print_r(gd_info()); PHP GD
 	echo'<br>';
 	for($i=0; $i<count($bitmap_array); $i++){ //1024
-		echo $bitmap_array[$i]; //ID 15とか0とか
+		//echo $bitmap_array[$i]; //ID 15とか0とか
 
 		imagecopy($createimage, $bit[$bitmap_array[$i]],($i%32)*15,intval($i/32)*15,0,0,15,15);
 	}
@@ -38,10 +38,7 @@
 		imagedestroy($createimage);
 ?>
 
-<html><head><link rel="stylesheet" id="coToolbarStyle" href="chrome-extension://cjabmdjcfcfdmffimndhafhblfmpjdpe/toolbar/styles/placeholder.css" type="text/css">
-<script type="text/javascript" id="cosymantecbfw_removeToolbar">
-(function () {				var toolbarElement = {},					parent = {},					interval = 0,					retryCount = 0,					isRemoved = false;				if (window.location.protocol === 'file:') {					interval = window.setInterval(function () {						toolbarElement = document.getElementById('coFrameDiv');						if (toolbarElement) {							parent = toolbarElement.parentNode;							if (parent) {								parent.removeChild(toolbarElement);								isRemoved = true;								if (document.body && document.body.style) {									document.body.style.setProperty('margin-top', '0px', 'important');								}							}						}						retryCount += 1;						if (retryCount > 10 || isRemoved) {							window.clearInterval(interval);						}					}, 10);				}			})();
-</script></head><body><center>
+<html><body><center>
 
 <title>ドット絵完成</title>
  	<link rel="stylesheet" type="text/css" href="style.css">
@@ -49,12 +46,9 @@
 <h2>完成！</h2>
 <!--$infoは$idに-->
 <img src=<?php echo "img/".$id.".gif"?>>
-<script>console.log("コンソールできたよ！");
-</script>
 <br><br>
-<br> また来てね！
 <br><br>
-<a href="sample6.php">新しく他の画像を作る</a>
+<a href="editor.php">新しく他の画像を作る</a>
 <a href="top.php">トップページに戻る</a>
 <br><br>
 </center>
--- top.php
+++ top.php
@@ -1,41 +1,23 @@
+<title>トップページ</title>
+<h1>ギャラリー</h1>
+<a href="editor.php">新しく画像を作る</a><br><br><br>
 <?php
   session_start();
 
   if (isset($_POST["bitmap"]) && isset($_POST["info"])) {
 	$bitmap = $_POST["bitmap"];
-	$info = $_POST["info"];
-	//＊＊＊認証処理＊＊＊
-	/*
-    $pdo = new PDO("sqlite:myblog.sqlite");
-    $st = $pdo->prepare("select * from user where username = ?;");
-    $st->execute(array($username));
-	$user_on_db = $st->fetch();
-	*/
+	$info = $_POST["info"];	
   }
-	/*
-  	echo "<pre>";
-	var_dump($bitmap); //var_dump 配列の中身を出力する
-	echo "</pre>";
-	*/
-/*
-  $db = new PDO("sqlite:works.sqlite");
-  $result=$db->query("SELECT * FROM works");
-  for($i=0; $row=$result->fetch(); $i++){
-      echo "<tr valign=center>";
-      echo "<td>". $row['id']."</td>";
-      echo "<td>". $row['info']."</td>";
-      echo "<td>". $row['user']."</td>";
-      echo "</tr>";
-  }*/
 
   $db = new PDO("sqlite:works.sqlite");
   $result=$db->query("SELECT * FROM works");
   for($i=0; $row=$result->fetch(); $i++){
-      echo "<tr valign=center>";?>
-      <img src= <?php echo "img/". $row['id'].".gif"?> width="150" height="150"><?php
-      echo "<br><td>". $row['info']."</td>";
-      echo "<br><td>". $row['user']."</td>";
-      echo "</tr>";
+      echo "<tr valign=center>";
+      echo "<td>タイトル". $row['info']."</td>";
+      echo "<td>作者". $row['user']."</td>";?>
+      <img src= <?php echo "img/". $row['id'].".gif"?> width="150" height="150">
+      </tr>
+      <?php
   }
 
   //画像読み込み
@@ -47,22 +29,3 @@
   }
 	echo'<br>';
 ?>
-
-<html><head><link rel="stylesheet" id="coToolbarStyle" href="chrome-extension://cjabmdjcfcfdmffimndhafhblfmpjdpe/toolbar/styles/placeholder.css" type="text/css">
-<script type="text/javascript" id="cosymantecbfw_removeToolbar">
-(function () {				var toolbarElement = {},					parent = {},					interval = 0,					retryCount = 0,					isRemoved = false;				if (window.location.protocol === 'file:') {					interval = window.setInterval(function () {						toolbarElement = document.getElementById('coFrameDiv');						if (toolbarElement) {							parent = toolbarElement.parentNode;							if (parent) {								parent.removeChild(toolbarElement);								isRemoved = true;								if (document.body && document.body.style) {									document.body.style.setProperty('margin-top', '0px', 'important');								}							}						}						retryCount += 1;						if (retryCount > 10 || isRemoved) {							window.clearInterval(interval);						}					}, 10);				}			})();
-</script></head><body><center>
-
-<title>ドット絵完成</title>
- 	<link rel="stylesheet" type="text/css" href="style.css">
-
-<h1>ギャラリー</h1>
-<!--$infoは$idに-->
-
-<script>console.log("コンソールできたよ！");
-</script>
-<br><br>
-<a href="sample6.php">新しく画像を作る</a>
-<br><br>
-</center>
-</body></html>
